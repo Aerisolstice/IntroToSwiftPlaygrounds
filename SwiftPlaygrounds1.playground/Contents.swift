fa2e866281341b5df08578fdfd709361f098cd07
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-01-17 10:40:33 -1000

    Going to merge branches

--- SwiftPlaygrounds1.playground/Contents.swift
+++ SwiftPlaygrounds1.playground/Contents.swift
@@ -22,5 +22,23 @@ var stillFollowing = " but I think it'll work out"
 var andStillFollowing = followAlong + stillFollowing
 print(andStillFollowing)
 
+//Assignment #2 Introduction to True & False
+var appState = false
 
+print(appState)
+
+
+//Comparing variables using > (greater than) or < (less than)
+//My initial attempt (I thought I should leave it in this time):
+var A = 3
+var B = 8
+var C = "var A < var B"
+
+//With the video:
+var numberOne = 5
+var numberTwo = 7
+
+//Create a variable that holds the comparision of the two variables we just created
+var compare = numberOne > numberTwo
+print(compare)
 
