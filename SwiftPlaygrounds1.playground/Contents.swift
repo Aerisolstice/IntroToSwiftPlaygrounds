2ce8e18444ec291c13e0309c2fc8545f66c3427e
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-01-16 17:17:06 -1000

    Completed Assignment #2 video
    Intro to Boolean Variables

--- SwiftPlaygrounds1.playground/Contents.swift
+++ SwiftPlaygrounds1.playground/Contents.swift
@@ -10,3 +10,31 @@ var mySecondVariable = " and look I added two strings together"
 
 var myThirdVariable = myFirstVariable + mySecondVariable
 print(myThirdVariable)
+
+//Assignment #2 Introduction to True & False
+
+var appState = false
+
+print(appState)
+
+
+//Comparing variables using > (greater than) or < (less than)
+
+//My initial attempt (I thought I should leave it in this time):
+var A = 3
+var B = 8
+var C = "var A < var B"
+
+//With the video:
+var numberOne = 5
+var numberTwo = 7
+
+//Create a variable that holds the comparision of the two variables we just created
+
+var compare = numberOne > numberTwo
+print(compare)
+
+
+
+
+
