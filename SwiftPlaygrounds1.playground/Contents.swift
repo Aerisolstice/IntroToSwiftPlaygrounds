f406e0310844e1e8b8d33c7f2aa214075b5233dd
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-01-17 10:50:07 -1000

    Probably Complete Now?

--- SwiftPlaygrounds1.playground/Contents.swift
+++ SwiftPlaygrounds1.playground/Contents.swift
@@ -1,25 +1,34 @@
 import UIKit
 
-var myFirstVariable: String
-myFirstVariable = "I've created my first variable"
+//var myFirstVariable: String
+var myFirstVariable = "I've created my first variable,"
 
-//We're going to do something with strings
 var mySecondVariable = " and look I added two strings together"
-
-
 
 var myThirdVariable = myFirstVariable + mySecondVariable
 print(myThirdVariable)
 
-//Assignment #2 Introduction to True & False
 
+
+//Swift Playgrounds #1
+
+//var followAlong: String
+var followAlong = "I hope I'm doing this right,"
+
+//var stillFollowing: String
+var stillFollowing = " but I think it'll work out"
+
+//var andStillFollowing: String
+var andStillFollowing = followAlong + stillFollowing
+print(andStillFollowing)
+
+//Assignment #2 Introduction to True & False
 var appState = false
 
 print(appState)
 
 
 //Comparing variables using > (greater than) or < (less than)
-
 //My initial attempt (I thought I should leave it in this time):
 var A = 3
 var B = 8
@@ -30,11 +39,6 @@ var numberOne = 5
 var numberTwo = 7
 
 //Create a variable that holds the comparision of the two variables we just created
-
 var compare = numberOne > numberTwo
 print(compare)
-
-
-
-
 
